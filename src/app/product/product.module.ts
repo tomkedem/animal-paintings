f5527fbf4bb26ed5632761d3d6b6085b0f944069
Tomer Kedem <tompha1@hotmail.com>
2023-12-22 17:49:45 +0200

    Creting mat-cards for products

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ]
 })
 export class ProductModule { }
