a86599c26d94958b14ff8cc6a56cddb171ba5ccc
Tomer Kedem <tompha1@hotmail.com>
2023-12-22 20:47:35 +0200

    Calculating the total cart price

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -8,15 +8,25 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./cart-view.component.css']
 })
 
-export class CartViewComponent implements OnInit{
+export class CartViewComponent implements OnInit {
 
-  cartItems: Product[] = []
+  cartItems: Product[] = [];
+  totalPrice: number = 0;
 
-  constructor(private cartService: CartService){}
-  
+  constructor(private cartService: CartService) { }
+
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     })
   }
+  getTotalPrice(): number {
+    let total = 0;
+    for (let item of this.cartItems) {
+      total += item.price;
+    }
+    return total;
+  }
+
 }
