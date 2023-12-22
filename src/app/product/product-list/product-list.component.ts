c0aabda1de5cc32ee93942679399d7eba35f1d82
Tomer Kedem <tompha1@hotmail.com>
2023-12-22 21:45:04 +0200

    Adding the sort functionality

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -3,7 +3,7 @@ import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
 import { CartService } from 'src/app/cart/cart.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { MatSelect } from '@angular/material/select';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -13,6 +13,8 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = []
   filteredProducts: Product[] = []
+  sortOrder: string = "";
+
   constructor(private productService: ProductService,
     private cartService: CartService, private snackbar: MatSnackBar) { }
 
@@ -42,5 +44,16 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.products.filter(
       product => product.name.toLocaleLowerCase().includes(searchTerm)
     )
+
+    this.sortProducts(this.sortOrder);
+  }
+
+  sortProducts(sortValue: string) {
+    this.sortOrder = sortValue;
+    if (this.sortOrder == "priceLowHigh") {
+      this.filteredProducts.sort((a,b) => a.price - b.price)
+    } else if (this.sortOrder == "priceHighLow") {
+      this.filteredProducts.sort((a,b) => b.price - a.price)
+    }
   }
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,6 +5,7 @@ import {MatCardModule} from '@angular/material/card';
 import { FlexModule } from '@angular/flex-layout';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import {MatInputModule} from '@angular/material/input';
     MatCardModule,
     FlexModule,
     MatSnackBarModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class ProductModule { }
